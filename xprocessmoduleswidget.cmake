9e55cb9b547575682d71771a8b95b255acc2ffbd
horsicq <horsicq@gmail.com>
2024-01-04 19:23:27 +0100

    Add new file(s): 2024-01-04

---
+++ xprocessmoduleswidget.cmake
@@ -0,0 +1,11 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+include(${CMAKE_CURRENT_LIST_DIR}/../XProcess/xprocess.cmake)
+
+set(XPROCESSMODULESWIDGET_SOURCES
+    ${XPROCESS_SOURCES}
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmodules.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmodules.ui
+    ${CMAKE_CURRENT_LIST_DIR}/xprocessmoduleswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xprocessmoduleswidget.ui
+)
