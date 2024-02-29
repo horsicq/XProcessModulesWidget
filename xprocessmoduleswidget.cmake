ce968c5ba2fae3e33f6b15f5162f34d013cf4a5a
horsicq <horsicq@gmail.com>
2024-03-01 00:06:37 +0100

    Update file(s): 2024-03-01

--- xprocessmoduleswidget.cmake
+++ xprocessmoduleswidget.cmake
@@ -5,7 +5,9 @@ include(${CMAKE_CURRENT_LIST_DIR}/../XProcess/xprocess.cmake)
 set(XPROCESSMODULESWIDGET_SOURCES
     ${XPROCESS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmodules.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmodules.h
     ${CMAKE_CURRENT_LIST_DIR}/dialogxprocessmodules.ui
     ${CMAKE_CURRENT_LIST_DIR}/xprocessmoduleswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xprocessmoduleswidget.h
     ${CMAKE_CURRENT_LIST_DIR}/xprocessmoduleswidget.ui
 )
